026c201b9842342e842ea1c46f4aa6b6609ddd2d
carapaceonbear <seerhan17@gmail.com>
2022-06-07 16:16:38 +0100

    tickets generated with a map,  single call in app

--- src/App.jsx
+++ src/App.jsx
@@ -8,16 +8,7 @@ const App = () => {
     <div className="App">
       <h1 class="title">Ticket Tracker</h1>
       <div class="ticket-display">
-        <Ticket employee={team[0]}/>
-        <Ticket employee={team[1]}/>
-        <Ticket employee={team[2]}/>
-        <Ticket employee={team[3]}/>
-        <Ticket employee={team[4]}/>
-        <Ticket employee={team[5]}/>
-        <Ticket employee={team[6]}/>
-        <Ticket employee={team[7]}/>
-        <Ticket employee={team[8]}/>
-        <Ticket employee={team[9]}/>
+        <Ticket employeeArray={team}/>
       </div>
     </div>
   );
--- src/components/Ticket/Ticket.jsx
+++ src/components/Ticket/Ticket.jsx
@@ -2,21 +2,26 @@ import React from 'react';
 import "./Ticket.scss";
 
 const Ticket = (props) => {
-    const {employee} = props;
+    const {employeeArray} = props;
+    const employeeList = employeeArray.map((employee => {
+        return (
+            <div class="ticket">
+                <h2 class="ticket__name">{employee.name}</h2>
+                <h2 class="ticket__role">{employee.role}</h2>
+                <div class="ticket__counter">
+                    <h2 class="ticket__counter--text">Counter</h2> 
+                    <h2 class="ticket__counter--number">0</h2>
+                    <div class="ticket__counter--button-box">
+                        <button class="ticket__counter--button" id="button-minus">-</button> 
+                        <button class="ticket__counter--button" id="button-plus">+</button>  
+                    </div>
+                </div>   
+            </div>
+        )
+    }));
 
     return (
-        <div class="ticket">
-            <h2 class="ticket__name">{employee.name}</h2>
-            <h2 class="ticket__role">{employee.role}</h2>
-            <div class="ticket__counter">
-                <h2 class="ticket__counter--text">Counter</h2> 
-                <h2 class="ticket__counter--number">0</h2>
-                <div class="ticket__counter--button-box">
-                    <button class="ticket__counter--button" id="button-minus">-</button> 
-                    <button class="ticket__counter--button" id="button-plus">+</button>  
-                </div>
-            </div>    
-        </div>
+        <>{employeeList}</>
     )
 }
 
