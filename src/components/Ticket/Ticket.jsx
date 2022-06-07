317baf15a5a241640e7367be3ab7e80da3153f48
carapaceonbear <seerhan17@gmail.com>
2022-06-07 16:06:28 +0100

    ticket styling

--- src/components/Ticket/Ticket.jsx
+++ src/components/Ticket/Ticket.jsx
@@ -11,8 +11,10 @@ const Ticket = (props) => {
             <div class="ticket__counter">
                 <h2 class="ticket__counter--text">Counter</h2> 
                 <h2 class="ticket__counter--number">0</h2>
-                <button class="ticket__counter--button" id="button-minus">-</button> 
-                <button class="ticket__counter--button" id="button-plus">+</button>  
+                <div class="ticket__counter--button-box">
+                    <button class="ticket__counter--button" id="button-minus">-</button> 
+                    <button class="ticket__counter--button" id="button-plus">+</button>  
+                </div>
             </div>    
         </div>
     )
