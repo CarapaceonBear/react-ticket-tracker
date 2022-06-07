84843b17d2abee153924b4c20836f13dcf941380
carapaceonbear <seerhan17@gmail.com>
2022-06-07 15:28:54 +0100

    file/folder structure

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.scss';
+import Ticket from './components/Ticket/Ticket';
+import team from './data/team';
+
+function App() {
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
