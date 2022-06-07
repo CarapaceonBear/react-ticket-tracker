b583947621f499d3f15ba9c51b82d4b94c94db78
carapaceonbear <seerhan17@gmail.com>
2022-06-07 15:44:33 +0100

    tickets for each employee in app

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,23 @@ import './App.scss';
 import Ticket from './components/Ticket/Ticket';
 import team from './data/team';
 
-function App() {
+const App = () => {
+
   return (
     <div className="App">
-
+      <h1 class="title">Ticket Tracker</h1>
+      <div class="ticket-display">
+        <Ticket employee={team[0]}/>
+        <Ticket employee={team[1]}/>
+        <Ticket employee={team[2]}/>
+        <Ticket employee={team[3]}/>
+        <Ticket employee={team[4]}/>
+        <Ticket employee={team[5]}/>
+        <Ticket employee={team[6]}/>
+        <Ticket employee={team[7]}/>
+        <Ticket employee={team[8]}/>
+        <Ticket employee={team[9]}/>
+      </div>
     </div>
   );
 }
--- src/components/Ticket/Ticket.jsx
+++ src/components/Ticket/Ticket.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import "./Ticket.scss";
 
 const Ticket = (props) => {
-    const {name, role} = props;
+    const {employee} = props;
 
     return (
         <div class="ticket">
-            <h2 class="ticket__name">{name}</h2>
-            <h2 class="ticket__role">{role}</h2>
+            <h2 class="ticket__name">{employee.name}</h2>
+            <h2 class="ticket__role">{employee.role}</h2>
             <div class="ticket__counter">
                 <h2 class="ticket__counter--text">Counter</h2> 
                 <h2 class="ticket__counter--number">0</h2>
