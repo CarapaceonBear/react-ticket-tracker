d70fd0543f273e723c275fa1be9e180e039179d8
carapaceonbear <seerhan17@gmail.com>
2022-06-08 12:49:57 +0100

    each ticket is individual, all called using an array

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,18 @@ import './App.scss';
 import Ticket from './components/Ticket/Ticket';
 import team from './data/team';
 
+const employees = team.map((employee) => {
+  return <Ticket name={employee.name} role={employee.role} />
+})
+
+
 const App = () => {
 
   return (
     <div className="App">
       <h1 className="title">Ticket Tracker</h1>
       <div className="ticket-display">
-        <Ticket employeeArray={team}/>
+        {employees}
       </div>
     </div>
   );
--- src/components/Ticket/Ticket.jsx
+++ src/components/Ticket/Ticket.jsx
@@ -2,33 +2,26 @@ import React, {useState} from 'react';
 import "./Ticket.scss";
 
 const Ticket = (props) => {
-    const {employeeArray} = props;
+    const {name, role} = props;
     const [counter, setCounter] = useState(0);
 
     const updateCounter = (event) => {
         event.target.value === "+" ? setCounter(counter + 1) : setCounter(counter - 1);
     }
 
-    const employeeList = employeeArray.map((employee => {
-        return (
-            <div className="ticket">
-                <h2 className="ticket__name">{employee.name}</h2>
-                <h2 className="ticket__role">{employee.role}</h2>
-                <div className="ticket__counter">
-                    <h2 className="ticket__counter--text">Counter</h2> 
-                    <h2 className="ticket__counter--number">{counter}</h2>
-                    <div className="ticket__counter--button-box">
-                        <button className="ticket__counter--button" id="button-minus" onClick={updateCounter} value="-">-</button> 
-                        <button className="ticket__counter--button" id="button-plus" onClick={updateCounter} value="+">+</button>  
-                    </div>
-                </div>   
-            </div>
-        )
-    }));
-
-
     return (
-        <>{employeeList}</>
+        <div className="ticket">
+            <h2 className="ticket__name">{name}</h2>
+            <h2 className="ticket__role">{role}</h2>
+            <div className="ticket__counter">
+                <h2 className="ticket__counter--text">Counter</h2> 
+                <h2 className="ticket__counter--number">{counter}</h2>
+                <div className="ticket__counter--button-box">
+                    <button className="ticket__counter--button" id="button-minus" onClick={updateCounter} value="-">-</button> 
+                    <button className="ticket__counter--button" id="button-plus" onClick={updateCounter} value="+">+</button>  
+                </div>
+            </div>   
+        </div>
     )
 }
 
