b9bd01f70ac1eb9373eecf362de760892ba4245b
carapaceonbear <seerhan17@gmail.com>
2022-06-08 14:53:02 +0100

    styling, colour change on positive counter

--- src/components/Ticket/Ticket.jsx
+++ src/components/Ticket/Ticket.jsx
@@ -4,7 +4,7 @@ import "./Ticket.scss";
 const Ticket = (props) => {
     const {name, role} = props;
     const [counter, setCounter] = useState(0);
-
+    
     const updateCounter = (event) => {
         if (event.target.value === "+") {
             setCounter(counter + 1);
@@ -12,12 +12,24 @@ const Ticket = (props) => {
             setCounter(counter - 1);
         }
     }
+    
+    let counterClass = "ticket__counter";
+    
+    const updateCounterClass = () => {
+        if (counter > 0) {
+            counterClass = "ticket__counter ticket__counter--positive";
+        } else {
+            counterClass = "ticket__counter";
+        }
+    }
+    
+    updateCounterClass();
 
     return (
         <div className="ticket">
             <h2 className="ticket__name">{name}</h2>
             <h2 className="ticket__role">{role}</h2>
-            <div className="ticket__counter">
+            <div className={counterClass}>
                 <h2 className="ticket__counter--text">Counter</h2> 
                 <h2 className="ticket__counter--number">{counter}</h2>
                 <div className="ticket__counter--button-box">
